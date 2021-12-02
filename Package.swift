bb6116fd7f4bac01fb855078983efd6632267c34
runner <runner@Mac-1638444099280.local>
2021-12-02 12:39:44 +0000

    [no ci] Bump tdlib 1.7.9-1d3cf2c2

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TDLibFramework",
-            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.7.9-5dc5b473/TDLibFramework.zip",
-            checksum: "61ce97e4233fcf38344ffb99495e0404e1f68cb5fc0ad5cd8ccc4c48f22c9cba"
+            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.7.9-1d3cf2c2/TDLibFramework.zip",
+            checksum: "422aba1382040b9f9b2f92c511add72e42aaf757b3721a634cc20beedfa0b397"
         ),
     ]
 )
