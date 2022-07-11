28cb8791cbe098117526d3ccad766bf7a0094058
runner <runner@Mac-1657540609555.local>
2022-07-11 12:04:35 +0000

    [no ci] Bump tdlib 1.8.4-1dec0e20

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TDLibFramework",
-            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.8.4-d4890143/TDLibFramework.zip",
-            checksum: "a0b021d37867dd3dc02fcd07ec3a3ed8d935053c6bf3a4e99079b06272f26900"
+            url: "https://github.com/Swiftgram/TDLibFramework/releases/download/1.8.4-1dec0e20/TDLibFramework.zip",
+            checksum: "a46c5b4caad368793f3a37fbb043a8ba37f2a3913462afee4a01ef68ea5ef658"
         ),
     ]
 )
