9d7329a1bb271f77555f05998fbcbb686c48819e
Sergey <iceman9831@gmail.com>
2022-07-14 09:57:24 +0300

    Parse tdlib version from cmakelists

--- scripts/extract_td_version.py
+++ scripts/extract_td_version.py
@@ -3,20 +3,20 @@ import argparse
 
 def parse_version(file_path: str) -> str:
     """
-        Parse Tdlib version from header file
+        Parse Tdlib version from CmakeLists.txt file
     """
     with open(file_path) as file:
         content = file.read()
 
-        m = re.search("TDLIB_VERSION = \"(.*)\";", content)
+        m = re.search("project\(TDLib VERSION (.*) LANGUAGES CXX C\)", content)
         if m:
             return(m.group(1))
         else:
-            raise ValueError("Unable to find TDLIB_VERSION in " + file_path)
+            raise ValueError("Unable to find TDLIB VERSION in " + file_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("header_path", help="Path to td/td/telegram/Td.h header file")
+    parser.add_argument("source_path", help="Path to td/CMakeLists.txt souce file")
     
     args = parser.parse_args()
-    print(parse_version(args.header_path))
+    print(parse_version(args.source_path))
